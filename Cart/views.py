0896971135f002c2b9605bed1fcd66623b48f7e5
UsamaHussain8 <usamahussain00@gmail.com>
2024-11-06 23:16:57 +0500

    Working app with PostgreSQL DB, some issues with signing up with google

--- Cart/views.py
+++ Cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect # type: ignore
 from .models import Cart, CartItem
 from Products.models import Product
 
--- MiniECommerce/settings.py
+++ MiniECommerce/settings.py
@@ -15,12 +15,16 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ # type: ignore
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e@iz9co4uqxrj2apn0&dox_5rc4%u%iix^nah#11n*i+1*+-74'
+SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,8 +126,12 @@ WSGI_APPLICATION = 'MiniECommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
 
